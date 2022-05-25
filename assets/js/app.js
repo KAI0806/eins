bcbcf1815ebfab804f2349fb7d6eae968b569932
甲斐聖健 <>
2022-05-25 12:53:54 +0900

    細部調整

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+$('.header_btn').on('click', function () {
+    $('.is-active').removeClass('is-active');
+    $(this).addClass('is-active');
+});
---
+++ assets/js/scrollify.js
@@ -0,0 +1,18 @@
+var current;
+$.scrollify({
+    section:".scrollify",
+    easing: 'easeOutExpo', // イージングを指定
+    scrollSpeed: 100, // スクロール時の速度
+    updateHash: false, // スクロール時アドレスバーのURLを更新
+    setHeights:false,
+    before:function(i,box){
+        current = i;
+    },
+});
+
+$(window).on('resize',function(){
+    if(current){
+        var currentScrl = $('.scrollify').eq(current).offset().top;
+        $(window).scrollTop(currentScrl);
+    }
+});
